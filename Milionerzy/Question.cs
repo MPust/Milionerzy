870baa20efe051d3336cc3b9a8253768e1b59788
Unknown <mateusz.pusty.pustelniak@gmail.com>
2017-08-22 21:17:33 +0200

    Dodanie Repozytorium pytań

--- Milionerzy/Question.cs
+++ Milionerzy/Question.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Milionerzy
 {
+
+
     class Question
     {
+        public int ID { get; set; }
+
+        public string  Text { get; set; }
+        public string AnswerA { get; set; }
+        public string AnswerB { get; set; }
+        public string AnswerC { get; set; }
+        public string AnswerD { get; set; }
+
+
     }
+
 }
---
+++ Milionerzy/QuestionRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milionerzy
+{
+    class QuestionRepository
+    {
+        private readonly Random random = new Random();
+
+        public Question GetNextQuestion()
+        {
+            int questionIndex = random.Next(questions.Count);
+
+            return questions[questionIndex];
+        }
+
+        private List<Question> questions = new List<Question>()
+        {
+            new Question()
+            {
+                ID = 1,
+                Text = "Jakiego koloru jest chaber bławatek?",
+                AnswerA = "Żóltego",
+                AnswerB = "Białego",
+                AnswerC = "Niebieskiego",
+                AnswerD = "Czerwonego",
+            },
+            new Question()
+            {
+                ID = 2,
+                Text = "Które z wymienionych nie jest rybą?",
+                AnswerA = "Rekin",
+                AnswerB = "Delfin",
+                AnswerC = "Flądra",
+                AnswerD = "MAkrela",
+            },
+            new Question()
+            {
+                ID = 3,
+                Text = "Króre z poniższych nie jest owadem?",
+                AnswerA = "Jelonek Rogacz",
+                AnswerB = "Tygrzyk paskowany",
+                AnswerC = "Czubajka",
+                AnswerD = "Komar",
+            },
+            new Question()
+            {
+                ID = 4,
+                Text = "Które anime nie należy do gatunku Harem?",
+                AnswerA = "Sword Art Online",
+                AnswerB = "Kono Subarashii Sekai ni Shukufu wo!",
+                AnswerC = "Date a Live",
+                AnswerD = "No.6",
+            },
+            new Question()
+            {
+                ID = 5,
+                Text = "Która z wymienionych nie dotyczy zarządzania projektami?",
+                AnswerA = "SCRUM",
+                AnswerB = "Kanban",
+                AnswerC = "Agile",
+                AnswerD = "SOLID",
+            },
+         };
+    }
+}
